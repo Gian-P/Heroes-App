fadb84999bd239211c78e31a4f2f001d08c561bb
Gian <pimentel.giancarlo1@gmail.com>
2022-10-18 19:56:53 -0400

    Tercer Commit

---
+++ src/auth/types/types.js
@@ -0,0 +1,6 @@
+// En este archivo tendremos los nombres de las acciones que podremos disparar en esta aplicacion.
+
+export const types = {
+  login:  '[Auth] Login',
+  logout: '[Auth] Logout'
+}
---
+++ src/heroes/pages/index.js
@@ -0,0 +1,4 @@
+export * from '../pages/DcPage';
+export * from '../pages/MarvelPage';
+export * from '../pages/SearchPage';
+export * from './HeroPage';
